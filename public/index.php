a66677dc05e3fabafb53c3c4c391ea2b41ce4106
Lorna Jane Mitchell <lorna@lornajane.net>
2019-01-09 11:25:40 +0000

    Add basic index and check route

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new Slim\App();
+
+// receive and return some data here
+$app->map(['GET', 'POST'], '/check', function ($request, $response, $args) {
+    // query parameters
+    $get_params = $request->getQueryParams();
+    // body params, using Content-Type to figure out if it's form or JSON
+    $post_params = $request->getParsedBody();
+
+    // accept header sets response format
+    $accept = $request->getHeader('Accept')[0];
+
+    // build the array of parameters and sources
+    $all_params = [];
+
+    if ($get_params) {
+        $all_params['GET'] = $get_params;
+    }
+
+    if ($post_params) {
+        $all_params['POST'] = $post_params;
+    }
+
+    switch ($accept) {
+        case 'application/json':
+            return $response->withJson($all_params);
+            break;
+        default: // plain text
+            return $response->getBody()->write(print_r($all_params, true));
+    }
+
+});
+
+$app->get('/', function ($request, $response, $args) {
+    return $response->getBody()->write("Hello World");
+});
+
+$app->run();
